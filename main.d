c0f2ab87f3dae395d91bcd86ff810a19e33faffe
Alan Chen <alanchen1908@gmail.com>
2019-11-26 08:45:46 -0500

    Add files via upload

---
+++ main.d
@@ -0,0 +1,84 @@
+..\main.d: ..\main.cpp \
+ O:\fehqt\Proteus/EWL/EWL_C++/include/lib_ewl_c++.prefix \
+ O:\fehqt\Proteus/EWL/EWL_C/include/lib_ewl.prefix \
+ O:\fehqt\Proteus/EWL/EWL_C/include/common.prefix \
+ O:\fehqt\Proteus/EWL/EWL_C/include/ewl_c_version.h Libraries/FEHLCD.h \
+ Drivers/spi.h ../fehproteusfirmware/Startup/derivative.h \
+ Startup/MK60DZ10.h O:\fehqt\Proteus/EWL/EWL_C/include/stdint.h \
+ O:\fehqt\Proteus/EWL/EWL_C/include/ansi_parms.h \
+ O:\fehqt\Proteus/EWL/EWL_C/include/ewlGlobals.h \
+ O:\fehqt\Proteus/EWL/EWL_C/include/arm/ansi_prefix.ARM.h \
+ O:\fehqt\Proteus/EWL/EWL_C/include/os_enum.h \
+ O:\fehqt\Proteus/EWL/EWL_C/include/cstdint \
+ O:\fehqt\Proteus/EWL/EWL_C/include/limits_api.h \
+ O:\fehqt\Proteus/EWL/EWL_C/include/ewl_lib_ext1.h Libraries/FEHUtility.h \
+ Startup/derivative.h Libraries/LCDColors.h Libraries/FEHIO.h \
+ Drivers/adc16.h ..\/tile.h ..\/grid.h ..\/tile.h ..\/vectorlight.h \
+ O:\fehqt\Proteus/EWL/EWL_C/include/stdlib.h \
+ O:\fehqt\Proteus/EWL/EWL_C/include/cstdlib \
+ O:\fehqt\Proteus/EWL/EWL_C/include/div_t.h \
+ O:\fehqt\Proteus/EWL/EWL_C/include/null.h \
+ O:\fehqt\Proteus/EWL/EWL_C/include/size_t.h \
+ O:\fehqt\Proteus/EWL/EWL_C/include/ewl_rsize_t.h
+
+O:\fehqt\Proteus/EWL/EWL_C++/include/lib_ewl_c++.prefix:
+
+O:\fehqt\Proteus/EWL/EWL_C/include/lib_ewl.prefix:
+
+O:\fehqt\Proteus/EWL/EWL_C/include/common.prefix:
+
+O:\fehqt\Proteus/EWL/EWL_C/include/ewl_c_version.h:
+
+Libraries/FEHLCD.h:
+
+Drivers/spi.h:
+
+../fehproteusfirmware/Startup/derivative.h:
+
+Startup/MK60DZ10.h:
+
+O:\fehqt\Proteus/EWL/EWL_C/include/stdint.h:
+
+O:\fehqt\Proteus/EWL/EWL_C/include/ansi_parms.h:
+
+O:\fehqt\Proteus/EWL/EWL_C/include/ewlGlobals.h:
+
+O:\fehqt\Proteus/EWL/EWL_C/include/arm/ansi_prefix.ARM.h:
+
+O:\fehqt\Proteus/EWL/EWL_C/include/os_enum.h:
+
+O:\fehqt\Proteus/EWL/EWL_C/include/cstdint:
+
+O:\fehqt\Proteus/EWL/EWL_C/include/limits_api.h:
+
+O:\fehqt\Proteus/EWL/EWL_C/include/ewl_lib_ext1.h:
+
+Libraries/FEHUtility.h:
+
+Startup/derivative.h:
+
+Libraries/LCDColors.h:
+
+Libraries/FEHIO.h:
+
+Drivers/adc16.h:
+
+..\/tile.h:
+
+..\/grid.h:
+
+..\/tile.h:
+
+..\/vectorlight.h:
+
+O:\fehqt\Proteus/EWL/EWL_C/include/stdlib.h:
+
+O:\fehqt\Proteus/EWL/EWL_C/include/cstdlib:
+
+O:\fehqt\Proteus/EWL/EWL_C/include/div_t.h:
+
+O:\fehqt\Proteus/EWL/EWL_C/include/null.h:
+
+O:\fehqt\Proteus/EWL/EWL_C/include/size_t.h:
+
+O:\fehqt\Proteus/EWL/EWL_C/include/ewl_rsize_t.h:
---
+++ vectorlight.d
@@ -0,0 +1,57 @@
+..\vectorlight.d: ..\vectorlight.cpp \
+ O:\fehqt\Proteus/EWL/EWL_C++/include/lib_ewl_c++.prefix \
+ O:\fehqt\Proteus/EWL/EWL_C/include/lib_ewl.prefix \
+ O:\fehqt\Proteus/EWL/EWL_C/include/common.prefix \
+ O:\fehqt\Proteus/EWL/EWL_C/include/ewl_c_version.h ..\/vectorlight.h \
+ O:\fehqt\Proteus/EWL/EWL_C/include/stdlib.h \
+ O:\fehqt\Proteus/EWL/EWL_C/include/ansi_parms.h \
+ O:\fehqt\Proteus/EWL/EWL_C/include/ewlGlobals.h \
+ O:\fehqt\Proteus/EWL/EWL_C/include/arm/ansi_prefix.ARM.h \
+ O:\fehqt\Proteus/EWL/EWL_C/include/os_enum.h \
+ O:\fehqt\Proteus/EWL/EWL_C/include/cstdlib \
+ O:\fehqt\Proteus/EWL/EWL_C/include/div_t.h \
+ O:\fehqt\Proteus/EWL/EWL_C/include/null.h \
+ O:\fehqt\Proteus/EWL/EWL_C/include/size_t.h \
+ O:\fehqt\Proteus/EWL/EWL_C/include/ewl_lib_ext1.h \
+ O:\fehqt\Proteus/EWL/EWL_C/include/ewl_rsize_t.h \
+ O:\fehqt\Proteus/EWL/EWL_C/include/stdint.h \
+ O:\fehqt\Proteus/EWL/EWL_C/include/cstdint \
+ O:\fehqt\Proteus/EWL/EWL_C/include/limits_api.h
+
+O:\fehqt\Proteus/EWL/EWL_C++/include/lib_ewl_c++.prefix:
+
+O:\fehqt\Proteus/EWL/EWL_C/include/lib_ewl.prefix:
+
+O:\fehqt\Proteus/EWL/EWL_C/include/common.prefix:
+
+O:\fehqt\Proteus/EWL/EWL_C/include/ewl_c_version.h:
+
+..\/vectorlight.h:
+
+O:\fehqt\Proteus/EWL/EWL_C/include/stdlib.h:
+
+O:\fehqt\Proteus/EWL/EWL_C/include/ansi_parms.h:
+
+O:\fehqt\Proteus/EWL/EWL_C/include/ewlGlobals.h:
+
+O:\fehqt\Proteus/EWL/EWL_C/include/arm/ansi_prefix.ARM.h:
+
+O:\fehqt\Proteus/EWL/EWL_C/include/os_enum.h:
+
+O:\fehqt\Proteus/EWL/EWL_C/include/cstdlib:
+
+O:\fehqt\Proteus/EWL/EWL_C/include/div_t.h:
+
+O:\fehqt\Proteus/EWL/EWL_C/include/null.h:
+
+O:\fehqt\Proteus/EWL/EWL_C/include/size_t.h:
+
+O:\fehqt\Proteus/EWL/EWL_C/include/ewl_lib_ext1.h:
+
+O:\fehqt\Proteus/EWL/EWL_C/include/ewl_rsize_t.h:
+
+O:\fehqt\Proteus/EWL/EWL_C/include/stdint.h:
+
+O:\fehqt\Proteus/EWL/EWL_C/include/cstdint:
+
+O:\fehqt\Proteus/EWL/EWL_C/include/limits_api.h:
